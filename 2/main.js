c4e0ee7b65ce2c1ae51c0c9ec106dccdb14c11d2
Bulat <zoko555555555555@gmail.com>
2025-09-20 14:49:26 +0300

    main.js

--- 2/main.js
+++ 2/main.js
@@ -1,3 +1,17 @@
-#hello world!
-const productReduce = numbers.reduce((acc, num) => acc * num, 1);
-console.log(productReduce); // 120
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<script>
+let num1 = 50;
+let num2 = 10;
+let ответ = num1 / num2;
+
+console.log(`${num1} / ${num2} = ${ответ}`); // 50 / 10 = 5 
+</script>
+</body>
+</html>
